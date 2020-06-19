ec95e2e19f0ccde2bf7fb6fb4d6a6f06dcdcffc7
Mario Lozano <mario_x_lozano@glic.com>
2020-06-19 10:10:40 -0400

    Added Christys playlist.ejs code

--- routes/playlists.js
+++ routes/playlists.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-router.get('/playlists', ctrl.playlists.renderPlaylist);
+router.get('/', ctrl.playlists.renderPlaylist);
 
 
 module.exports = router;
